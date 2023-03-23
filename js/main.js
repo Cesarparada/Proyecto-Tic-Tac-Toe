f9bcb4485a28625f0c6f6011343f33c27bd167ef
CesarParada <cesard.0925@gmail.com>
2023-03-23 20:10:26 +0100

    diseñada la vista inicio en main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,72 @@
+const root = document.getElementById("root");
+
+///////Vista Inicio///////
+
+const getInicioHTML= () => {
+  return `
+  <div class="container-home">
+      <div class="title-home"><h1>Bienvenido a Tic Tac Món</h1></div>
+      <div class="card">
+        <div class="face front">
+          <h2>Información del Juego</h2>
+          <p>
+            En Tic Tac Toe los jugadores se turnan para completar con sus
+            propias fichas “X” u “O”, en la cuadrícula de tres cuadros para
+            completar la secuencia de fichas y adelantarse a su oponente,
+            ganando así el juego.
+          </p>
+          <div>
+            <img class="items" src="multimedia/caterpie.png" alt="caterpie" />
+          </div>
+        </div>
+        <div class="face back">
+          <h2>Instrucciones del Juego</h2>
+          <h4>Judar vs Jugador</h4>
+          <p>
+            Escribe tu nombre y selecciona tu ficha para juegar contra otros
+            jugadores, logrando hacer una secuencia de tres fichas para ganar.
+          </p>
+          <h4>Jugador vs CPU</h4>
+          <p>
+            Escribe tu nombre y selecciona tu ficha para juegar contra la
+            maquina, logrando hacer una secuencia de tres de tus fichas le
+            podras ganar.
+          </p>
+          <div>
+            <img class="items" src="multimedia/squirtle.png" alt="squirtle" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <input type="checkbox" id="btn-modal" />
+        <label for="btn-modal" class="lbl-modal">Play</label>
+        <div class="modal">
+          <div class="contenedor">
+            <header>Modo de Juego</header>
+            <label for="btn-modal">x</label>
+            <div class="contenido">
+              <div>
+                <a href="index.html">
+                  <img
+                    class="players"
+                    src="multimedia/players.png"
+                    alt="Players"
+                /></a>
+              </div>
+              <div>
+                <a href="index.html">
+                  <img class="players" src="multimedia/robot.png" alt="Robot"
+                /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+
 const htmlCasillas = document.querySelectorAll(".casilla");
 const mapCasillas = [];
 let turno1 = false;
@@ -34,3 +103,11 @@ for (let i = 0; i < htmlCasillas.length; i++) {
 
   htmlCasillas[i].id = i;
 }
+//////Vista Inicio/////////
+
+const renderInicio = () => {
+    root.innerHTML = getInicioHTML();
+};
+//////Llamada a la Funcion//////
+
+renderInicio();
